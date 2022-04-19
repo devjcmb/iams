fa29635dea4ab508f6d0946052597c32876e8b36
Joe Carlo Boto <joe@fusedsoftware.com>
2022-04-19 21:40:13 +0800

    fix for redirecting to login  instead of giving json response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -46,5 +47,14 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['errors' => ['message' => 'Unauthenticated'], 'status' => false ], 401);
+        }
+
+        return redirect()->guest(route('auth.login'));
     }
 }
